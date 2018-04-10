16ea4afbbd911b43725cf5aa53bd2522903f104e
shadid12 <shadidhaque2012@gmail.com>
2018-04-10 12:34:10 -0400

    remake the files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MainSideNav from './Components/MainSideNav';
+
+export default class App extends Component {
+    render() {
+        return(
+            <div>
+                <MainSideNav />
+            </div>
+        )
+    }
+}
---
+++ src/Components/MainSideNav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class MainSideNav extends React.Component {
+    render() {
+        return(
+            <div>Main Side Nav</div>
+        )
+    }
+} 
