b6b949c437256a7860f224f7b78e0303a3004ce6
shadid12 <shadidhaque2012@gmail.com>
2018-04-10 15:20:46 -0400

    first nav column is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import MainSideNav from './Components/MainSideNav';
+import { Row, Col } from 'antd';
+
+import './App.css';
 
 export default class App extends Component {
     render() {
         return(
             <div>
-                <MainSideNav />
+                <Row>
+                    <Col span={2}>
+                        <MainSideNav />
+                    </Col>
+                    <Col span={12}>col-12</Col>
+                </Row>
             </div>
         )
     }
--- src/Components/MainSideNav.js
+++ src/Components/MainSideNav.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import { Row, Col, Icon, Avatar } from 'antd';
+
+import './css/main-side-nav.css';
 
 export default class MainSideNav extends React.Component {
     render() {
         return(
-            <div>Main Side Nav</div>
+            <div>
+                <Row>
+                    <div className="profile-img">
+                        <Avatar 
+                            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" 
+                            size="large"
+                        />
+                    </div>
+                </Row>
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="link" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="qq" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="github" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="google" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="alipay" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="yuque" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="hor-line"></div>
+                    <div className="icon-holders">
+                        <Icon type="dropbox" 
+                              style={{ fontSize: 30, color: '#08c' }}/>
+                    </div>
+                </Row>
+            </div>
         )
     }
 } 
+
