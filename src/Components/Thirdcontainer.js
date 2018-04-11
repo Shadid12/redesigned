8e13ba5238d99f30351499c9a781f1d858c30da4
shadid12 <shadidhaque2012@gmail.com>
2018-04-11 17:47:45 -0400

    added new users and stuff

--- src/Components/Thirdcontainer.js
+++ src/Components/Thirdcontainer.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Row } from 'antd';
 import { Input } from 'antd';
+import openSocket from 'socket.io-client';
+
 import MessageList from './MessageList';
 import './css/third-container.css';
 
 export default class Thirdcontainer extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            users_in_room: []
+        }
+        
+        this.socket = openSocket('localhost:5000', { query: "foo=bar" });
+
+        this.socket.on('JOINED_ROOM', (data) => {
+            console.log(data.user);
+        });
+    }
+
     render() {
         const Search = Input.Search;
 
